28d80cf85e2c7940793d9ebe028d9cdcc6c119e2
Scott Sickles <scottsickles@gmail.com>
2023-12-13 15:08:11 -0500

    fix name of variable

--- user-pool.tf
+++ user-pool.tf
@@ -8,13 +8,13 @@ locals {
   client_id     = data.ns_connection.user-pool.outputs.client_id
   web_client_id = data.ns_connection.user-pool.outputs.web_client_id
   access_key_id_secret_id = data.ns_connection.user-pool.outputs.access_key_id_secret_id
-  secret_access_token_secret_id = data.ns_connection.user-pool.outputs.secret_access_token_secret_id
+  secret_access_key_secret_id = data.ns_connection.user-pool.outputs.secret_access_key_secret_id
 }
 
 data "aws_secretsmanager_secret_version" "access_key_id" {
   secret_id = local.access_key_id_secret_id
 }
 
-data "aws_secretsmanager_secret_version" "secret_access_token" {
-  secret_id = local.secret_access_token_secret_id
+data "aws_secretsmanager_secret_version" "secret_access_key" {
+  secret_id = local.secret_access_key_secret_id
 }
