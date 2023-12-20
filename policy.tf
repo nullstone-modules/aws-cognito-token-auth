6ac53779c93adbdfa2290cf16f15a8055a1b8066
Scott Sickles <scottsickles@gmail.com>
2023-12-20 13:04:34 -0500

    only add the policy if there is a role

--- policy.tf
+++ policy.tf
@@ -1,4 +1,9 @@
+locals {
+  needs_access = contains(keys(var.app_metadata), "role_name")
+}
+
 resource "aws_iam_role_policy_attachment" "this" {
+  count      = local.needs_access ? 1 : 0
   role       = var.app_metadata["role_name"]
   policy_arn = data.aws_iam_policy.power-user.arn
 }
