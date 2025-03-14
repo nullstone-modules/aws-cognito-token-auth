7610fd1ea1476cb5dba55f4a81e0faa7d5c750f6
Brad Sickles <brad.sickles@gmail.com>
2025-03-14 15:48:12 -0400

    Handle when user pool has blank secrets

--- outputs.tf
+++ outputs.tf
@@ -23,11 +23,11 @@ output "secrets" {
     },
     {
       name  = "COGNITO_ACCESS_KEY_ID"
-      value = data.aws_secretsmanager_secret_version.access_key_id.secret_string
+      value = try(data.aws_secretsmanager_secret_version.access_key_id[0].secret_string, "")
     },
     {
       name  = "COGNITO_SECRET_ACCESS_KEY"
-      value = data.aws_secretsmanager_secret_version.secret_access_key.secret_string
+      value = try(data.aws_secretsmanager_secret_version.secret_access_key[0].secret_string, "")
     }
   ]
 }
--- user-pool.tf
+++ user-pool.tf
@@ -8,8 +8,8 @@ locals {
   client_id                   = data.ns_connection.user-pool.outputs.client_id
   identity_pool_id            = data.ns_connection.user-pool.outputs.identity_pool_id
   client_secret_secret_id     = try(data.ns_connection.user-pool.outputs.client_secret_secret_id, "")
-  access_key_id_secret_id     = data.ns_connection.user-pool.outputs.access_key_id_secret_id
-  secret_access_key_secret_id = data.ns_connection.user-pool.outputs.secret_access_key_secret_id
+  access_key_id_secret_id     = try(data.ns_connection.user-pool.outputs.access_key_id_secret_id, "")
+  secret_access_key_secret_id = try(data.ns_connection.user-pool.outputs.secret_access_key_secret_id, "")
 }
 
 data "aws_secretsmanager_secret_version" "client_secret" {
@@ -18,9 +18,11 @@ data "aws_secretsmanager_secret_version" "client_secret" {
 }
 
 data "aws_secretsmanager_secret_version" "access_key_id" {
+  count     = length(local.access_key_id_secret_id) > 0 ? 1 : 0
   secret_id = local.access_key_id_secret_id
 }
 
 data "aws_secretsmanager_secret_version" "secret_access_key" {
+  count     = length(local.secret_access_key_secret_id) > 0 ? 1 : 0
   secret_id = local.secret_access_key_secret_id
 }
