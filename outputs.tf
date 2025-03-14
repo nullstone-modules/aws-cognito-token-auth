bbd889d074bbd4812920e21e43856a6f2f45aedd
Brad Sickles <brad.sickles@gmail.com>
2025-03-14 16:08:11 -0400

    Do not emit outputs to secrets if no value

--- outputs.tf
+++ outputs.tf
@@ -15,19 +15,14 @@ output "env" {
   ]
 }
 
+locals {
+  cognito_secrets = {
+    "COGNITO_CLIENT_SECRET"     = try(data.aws_secretsmanager_secret_version.client_secret[0].secret_string, "")
+    "COGNITO_ACCESS_KEY_ID"     = try(data.aws_secretsmanager_secret_version.access_key_id[0].secret_string, "")
+    "COGNITO_SECRET_ACCESS_KEY" = try(data.aws_secretsmanager_secret_version.secret_access_key[0].secret_string, "")
+  }
+}
+
 output "secrets" {
-  value = [
-    {
-      name  = "COGNITO_CLIENT_SECRET"
-      value = try(data.aws_secretsmanager_secret_version.client_secret[0].secret_string, "")
-    },
-    {
-      name  = "COGNITO_ACCESS_KEY_ID"
-      value = try(data.aws_secretsmanager_secret_version.access_key_id[0].secret_string, "")
-    },
-    {
-      name  = "COGNITO_SECRET_ACCESS_KEY"
-      value = try(data.aws_secretsmanager_secret_version.secret_access_key[0].secret_string, "")
-    }
-  ]
+  value = [for k, v in local.cognito_secrets : { name = k, value = v } if v != ""]
 }
